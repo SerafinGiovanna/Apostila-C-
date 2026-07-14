7785ee963f5368900b1e67e8cbd7df04c42afbde
Giovanna Serafin <gioserafin281@gmail.com>
2026-07-14 17:02:33 -0300

    Conversao de Graus Celsius para Fahrenheit

---
+++ Exercicio7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int C, F;
+    cout << "Digite o grau Celsius: ";
+    cin >> C;
+    F = C*9/5+32;
+    cout << "Grau convertido em Fahrenheit: " << F << endl;
+}
