cd496f052bf43e38a42ea9a808a44f7e489c4da9
SerafinGiovanna <serafingiovanna068@gmail.com>
2026-07-21 17:01:50 -0300

    feat: Bloqueia o arquivo .ideia

---
+++ Apostila 01/ExercicioMicael.cpp
@@ -0,0 +1,5 @@
+//
+// Created by giovanna.santos on 21/07/2026.
+//
+
+#include "ExercicioMicael.h"
---
+++ Apostila 01/ExercicioMicael.h
@@ -0,0 +1,16 @@
+//
+// Created by giovanna.santos on 21/07/2026.
+//
+
+#ifndef APOSTILA_01_EXERCICIOMICAEL_H
+#define APOSTILA_01_EXERCICIOMICAEL_H
+
+
+
+class ExercicioMicael {
+
+};
+
+
+
+#endif //APOSTILA_01_EXERCICIOMICAEL_H
