296997bd620bd54d69045638c2889010422dc804
SerafinGiovanna <serafingiovanna068@gmail.com>
2026-07-31 16:54:42 -0300

    feat: Exercício 3 iniciado

---
+++ Apostila 02/03-Desenhista.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int P ;
+    char C = '#', C2 = '>';
+
+    cin >> P;
+
+    C2 -= 1;
+    C += 1;
+
+    for ()
+
+    cout << "#" << P;
+}
