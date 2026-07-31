9e3866403fbe0cd1dd2affc1e38ad2d2778dfffc
SerafinGiovanna <serafingiovanna068@gmail.com>
2026-07-31 13:50:02 -0300

    feat: Exercicio 02 feito

---
+++ Apostila 02/02-Continha.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int A, B, C, D, E, F;
+    double tot;
+
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+    cin >> E;
+    cin >> F;
+
+    tot = ((A+ B) * (C - D) * (E + F)) / 2;
+
+    cout << "Eu sou FERA nas continhas e o resutado e " << tot;
+
+}
