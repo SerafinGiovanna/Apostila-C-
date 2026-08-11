bf2a33aa460f0fb42099abf2997f57cd5c7bcfb5
SerafinGiovanna <serafingiovanna068@gmail.com>
2026-08-11 14:11:09 -0300

    feat: Exercicio 06 concluido

--- Apostila 02/06-Média Ponderada.cpp
+++ Apostila 02/06-Média Ponderada.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main () {
-    int nota1, nota2,
+    float nota1, nota2, nota3, media;
+
+    cin >>  nota1;
+    cin >> nota2;
+    cin >> nota3;
+
+    media = ((nota1 * 4) + (nota2 * 4 ) + (nota3 * 2)) / 10;
+
+    cout << fixed << setprecision(2);
+    cout << media << endl;
 }
